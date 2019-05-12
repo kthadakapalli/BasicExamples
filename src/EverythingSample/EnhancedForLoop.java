e8523e2190f89364a3073bb8a5a2ddde0a5b92ee
kthadakapalli <kthadakapalli@gmail.com>
2019-05-12 22:42:26 +0530

    second commit

---
+++ src/EverythingSample/EnhancedForLoop.java
@@ -0,0 +1,12 @@
+package EverythingSample;
+
+public class EnhancedForLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int test[] = { 1, 2, 2, 3, 4 };
+		for (int i : test)
+			System.out.println(i);
+	}
+
+}
